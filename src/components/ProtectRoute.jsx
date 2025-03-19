5b94194b35eae861caca39cf85338967707678ba
guinegrijo <guilhermenegrijo@gmail.com>
2025-03-19 10:33:08 -0300

    Projeto VIO

---
+++ src/components/ProtectRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute ({children}) {
+
+    const isAuthenticated = localStorage.getItem("authenticated")
+
+    return isAuthenticated ? children : <Navigate to="/" />
+}
+
+export default ProtectedRoute
